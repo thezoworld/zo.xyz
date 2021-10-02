58ea6306e6b8d769ecf475f85d48def0d2c5ab0d
manas9927 <manas@zo.xyz>
2021-10-02 18:36:49 +0530

    :construction: Restructured about page

--- src/components/page-sections/about/index.tsx
+++ src/components/page-sections/about/index.tsx
@@ -1,6 +1,4 @@
 import Mission from "./Mission";
-import Story from "./Story";
 import Team from "./Team";
-import Hiring from "./Hiring";
 
-export { Mission, Story, Team, Hiring };
+export { Mission, Team };
