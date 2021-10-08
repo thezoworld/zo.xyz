ccbd90249a23cd1a67979046464a7c1496bc60d5
Ishaan Rawat <ishaan@ishaanrawat.com>
2021-10-09 00:42:05 +0530

    ga fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import Script from "next/script";
 import "tailwindcss/tailwind.css";
 import { Footer, Header } from "../components/page-sections/common";
@@ -7,21 +6,17 @@ import { Footer, Header } from "../components/page-sections/common";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <script
-          async={true}
-          src="https://www.googletagmanager.com/gtm.js?id=GTM-PWPM6Z8"
-          dangerouslySetInnerHTML={{
-            __html: `
-          window['dataLayer'] = window['dataLayer'] || [];
-          window['dataLayer'].push({
-            'gtm.start': new Date().getTime(),
-            event: 'gtm.js',
-          });
-          `,
-          }}
-        ></script>
-      </Head>
+      <Script
+        id="gtm"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PWPM6Z8');`,
+        }}
+      ></Script>
       <main className="w-full h-full">
         <Header />
         <Component {...pageProps} />
