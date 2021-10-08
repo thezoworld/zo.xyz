3906e6106b926a07a20d0fdd1b7203db9bfc7c98
Ishaan Rawat <ishaan@ishaanrawat.com>
2021-10-08 22:54:27 +0530

    gtm fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,23 +7,11 @@ import { Footer, Header } from "../components/page-sections/common";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script
-        id="gtm"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function (w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-            var f = d.getElementsByTagName(s)[0],
-              j = d.createElement(s),
-              dl = l != "dataLayer" ? "&l=" + l : "";
-            j.async = true;
-            j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-            f.parentNode.insertBefore(j, f);
-          })(window, document, "script", "dataLayer", "GTM-WTMS2ZQ");
-          `,
-        }}
-      ></Script>
+      <Script strategy="beforeInteractive" id="gtm">
+        {`
+          (function (w, d, s, l, i) { w[l] = w[l] || [];w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });var f = d.getElementsByTagName(s)[0],j = d.createElement(s),dl = l != "dataLayer" ? "&l=" + l : "";j.async = true;j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;f.parentNode.insertBefore(j, f);})(window, document, "script", "dataLayer", "GTM-PWPM6Z8");
+          `}
+      </Script>
       <main className="w-full h-full">
         <Header />
         <Component {...pageProps} />
