bc24b82cda6213f84810283c45a6456fee6f21ef
manas9927 <manas@zo.xyz>
2021-10-14 16:18:04 +0530

    :art: Added progressbar

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,6 @@
 import BlogCard from "./BlogCard";
 import Button from "./Button";
 import { Container } from "./Container";
+import Progressbar from "./Progressbar";
 
-export { Button, Container, BlogCard };
+export { Button, Container, BlogCard, Progressbar };
