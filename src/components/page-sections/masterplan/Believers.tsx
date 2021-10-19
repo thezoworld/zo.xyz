5d71adaab5309890fc353fb93a7fa00b17eef015
manas9927 <manas@zo.xyz>
2021-10-19 21:07:07 +0530

    :lipstick: UI improvements and code cleanup in Masterplan

--- src/components/page-sections/masterplan/Believers.tsx
+++ src/components/page-sections/masterplan/Believers.tsx
@@ -10,27 +10,26 @@ const Believers: React.FC<BelieversProps> = () => {
   return (
     <section className="max-w-full">
       <Container
-        className="md:h-144 bg-masterplanBackground transform skew-y-6"
+        className="h-full bg-masterplanBackground transform skew-y-6"
         style={{ background: "#94AE31" }}
       >
-        <Flex col={false} className="md:mx-0 mx-3 transform -skew-y-6">
-          <Flex
-            col
-            className="h-full md:pb-32 pb-20 md:pt-20 pt-10 md:items-start items-center md:w-2/3 w-screen text-white"
-          >
-            <ZobuPLayingCards className="md:hidden block w-1/2 pt-5 order-2 md:order-1" />
-            <h1 className="md:text-2xl mt-8 text-left w-full px-8 md:px-0 md:leading-10 leading-7 text-lg md:mt-0 font-bold order-1 md:order-2">
-              Believers
-            </h1>
-            <p className="mt-8 md:text-left md:mt-8 px-8 md:px-0 mb-8 md:mb-0 order-1 md:order-2">
-              Those who believe in the Zo World vision will be able to showcase
-              their support by minting a 'Founders of the Zo World' NFT.
-              <br />
-              <br />
-              5,555 of the only 11,111 founders NFTs will be available for
-              public minting, 31st Oct 2021 onwards.
-            </p>
-          </Flex>
+        <Flex
+          col
+          justify="center"
+          className="h-full md:py-32 py-5 md:items-start items-center md:w-2/3 w-screen text-white md:mx-0 mx-3 transform -skew-y-6"
+        >
+          <ZobuPLayingCards className="md:hidden block w-1/3 py-5 order-3" />
+          <h1 className="md:text-2xl my-4 text-left w-full px-8 md:px-0 md:leading-10 leading-7 text-lg font-bold order-1">
+            Believers
+          </h1>
+          <p className="my-4 md:text-left px-8 md:px-0 order-2">
+            Those who believe in the Zo World vision will be able to showcase
+            their support by minting a 'Founders of the Zo World' NFT.
+            <br />
+            <br />
+            5,555 of the only 11,111 founders NFTs will be available for public
+            minting, 31st Oct 2021 onwards.
+          </p>
         </Flex>
       </Container>
     </section>
--- src/components/page-sections/masterplan/Builders.tsx
+++ src/components/page-sections/masterplan/Builders.tsx
@@ -9,25 +9,27 @@ interface BuildersProps {}
 const Builders: React.FC<BuildersProps> = () => {
   return (
     <Container
-      className="md:h-144 relative md:pt-32 pt-8 transform -skew-y-6"
+      className="h-full relative transform -skew-y-6"
       style={{ background: "#A1C468" }}
     >
-      <Flex className="md:flex-row flex-col md:mx-0 mx-3 transform skew-y-6 ">
+      <Flex className="md:flex-row flex-col md:mx-0 mx-3 transform skew-y-6 py-5 md:py-16">
         <Flex
           col
-          className="h-full md:pt-40 pt-5 md:pb-32 md:items-start items-center md:w-1/3 order-2 md:order-1"
+          className="h-full md:items-start items-center md:w-1/3 order-2 md:order-1"
         >
-          <ZobuPLayingCards className="md:block hidden md:absolute md:w-1/4 md:-top-full md:right-0 md:-mt-20" />
-          <ZobuWithGlobe2 className="w-screen md:w-3/4 md:absolute md:-left-3/4 md:-bottom-48 mr-8" />
+          {/* <ZobuPLayingCards className="md:block hidden md:absolute md:w-1/5 md:bottom-3/4 md:right-0 md:-mt-20" /> */}
+          <ZobuPLayingCards className="md:block hidden md:absolute w-1/5 bottom-2/3 right-0" />
+          <ZobuWithGlobe2 className="w-screen md:w-2/3 md:absolute md:-left-1/3 md:-top-10 mr-8" />
         </Flex>
         <Flex
           col
-          className="h-full md:pt-20 md:pb-40 pt-8 items-start md:w-2/3 text-white order-1 md:order-2"
+          // className="h-full md:pt-20 md:pb-40 pt-8 items-start md:w-2/3 text-white order-1 md:order-2"
+          className="h-full md:py-20 py-8 items-start md:w-2/3 text-white order-1 md:order-2"
         >
-          <h1 className="md:text-2xl text-left md:leading-10 md:px-0 px-8 leading-7 font-bold text-lg md:mt-8">
+          <h1 className="md:text-2xl text-left md:leading-10 md:px-0 px-8 leading-7 font-bold text-lg py-4">
             Builders
           </h1>
-          <p className="text-left mt-8 md:px-0 px-8">
+          <p className="text-left py-4 md:px-0 px-8">
             Travel hosts, designers, coders, HQ, Influencers and many other ways
             you can help build the global Zo travel ecosystem and become a
             founder of the Zo World.
