43a22f8654294b671bb2932233dfc3e9966a5a59
Vibhu Joshi <joshivibhu97@gmail.com>
2021-11-23 16:07:04 +0530

    Hooks for web3 and metamask wallet.

---
+++ src/components/hooks/useMetaMask.tsx
@@ -0,0 +1,79 @@
+import detectEthereumProvider from "@metamask/detect-provider";
+import MetaMaskOnboarding from "@metamask/onboarding";
+import { useEffect, useMemo, useRef, useState } from "react";
+
+function useMetaMask() {
+  const [ethereum, setEthereum] = useState<any>();
+  const onboarding = useRef<MetaMaskOnboarding>();
+  const [wallet, setWallet] = useState("");
+  const [chainId, setChainId] = useState("");
+
+  const isInstalled = useMemo(
+    () => !!(ethereum && ethereum.isMetaMask),
+    [ethereum]
+  );
+  const isConnected = useMemo(
+    () => !!(isInstalled && wallet),
+    [isInstalled, wallet]
+  );
+
+  const isListeningToAccounts = useRef(false);
+  const isListeningToChain = useRef(false);
+
+  const onboard = () => {
+    onboarding.current && onboarding.current.startOnboarding();
+  };
+
+  const handleAccountChanged = (_wallet: string) => setWallet(_wallet);
+
+  const connect = async () => {
+    if (isInstalled) {
+      const [_wallet] = await ethereum?.request({
+        method: "eth_requestAccounts",
+      });
+      handleAccountChanged(_wallet);
+    } else onboard();
+  };
+
+  useEffect(() => {
+    if (!onboarding.current) {
+      onboarding.current = new MetaMaskOnboarding();
+    }
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const _ethereum = (await detectEthereumProvider()) as any;
+      if (_ethereum && _ethereum.isMetaMask) {
+        setEthereum(_ethereum);
+        setWallet(_ethereum.selectedAddress || "");
+        setChainId(_ethereum.chainId || "");
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (isInstalled && !isListeningToAccounts.current) {
+      ethereum.on("accountsChanged", handleAccountChanged);
+      isListeningToAccounts.current = true;
+    }
+
+    return () => {
+      if (isListeningToAccounts.current) {
+        ethereum.removeListener("accountsChanged", handleAccountChanged);
+        isListeningToAccounts.current = false;
+      }
+    };
+  }, [isInstalled]);
+
+  useEffect(() => {
+    if (isInstalled && !isListeningToChain.current) {
+      ethereum.on("chainChanged", () => window.location.reload());
+      isListeningToChain.current = true;
+    }
+  }, [isInstalled]);
+
+  return { wallet, chainId, isInstalled, isConnected, connect };
+}
+
+export default useMetaMask;
---
+++ src/components/hooks/useWeb3.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useRef } from "react";
+import Web3 from "web3";
+
+function useWeb3() {
+  const web3Ref = useRef<Web3>();
+
+  useEffect(() => {
+    if (!web3Ref.current) {
+      web3Ref.current = new Web3(window.ethereum as any);
+    }
+  }, []);
+}
+
+export default useWeb3;
